1aec3763d8dbcb66abc228f75515ea659a685bb3
VanoKi <kirsvano@gmail.com>
2025-06-06 17:57:04 +0300

    fix: type of params deleteAffair

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -3,8 +3,8 @@ import Affairs from './affairs/Affairs'
 import s2 from '../../s1-main/App.module.css'
 
 /*
-* 1 - описать типы AffairPriorityType, AffairType
-* 2 - указать нужный тип для defaultAffairs
+* 1 - описать типы AffairPriorityType, AffairType - done
+* 2 - указать нужный тип для defaultAffairs - done
 * 3 - дописать типы и логику функции filterAffairs и проверить её тестами
 * 4 - выполнить пункт 3 для функции deleteAffair
 * 5 - указать нужный тип в useState с affairs
@@ -35,13 +35,15 @@ const defaultAffairs:AffairType[] = [ // need to fix any
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
-
-
-    return affairs // need to fix
+export const filterAffairs = (affairs: AffairType[], filter: FilterType): AffairType[] => { // need to fix any
+    if (filter === 'all') {
+        return affairs
+    } else {
+        return affairs.filter(el => el.priority === filter)
+    }
+    // return affairs // need to fix
 }
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
-
+export const deleteAffair = (affairs: AffairType[], _id: number): AffairType => { // need to fix any
     return affairs // need to fix
 }
 
