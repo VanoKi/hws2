c2718181e4170a19233570b168e77b41ed08d384
VanoKi <kirsvano@gmail.com>
2025-06-17 20:17:10 +0300

    feat: add start page

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -19,7 +19,7 @@ function Pages() {
       <Routes>
         {/*роутинг будут писать студенты*/}
         {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-        <Route/>
+        <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/> }/>
 
         {/*роуты для /pre-junior, /junior, /junior-plus*/}
         <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
@@ -27,7 +27,7 @@ function Pages() {
         <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
         {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-        <Route path={PATH.Error404} element={<Error404/>}/>
+        <Route path={'*/'} element={<Error404/>}/>
       </Routes>
     </div>
   )
